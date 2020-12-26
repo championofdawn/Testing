186c4ea80b7958ea2233a6ec11fd262b46a0e51b
championofdawn <62745914+championofdawn@users.noreply.github.com>
2020-12-26 23:30:41 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe778ae94657a0021e7dc34",
+				"5fe778ae94657a0021e7dc36"
+			]
+		},
+		"5fe778ae94657a0021e7dc34": {
+			"id": "5fe778ae94657a0021e7dc34",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe778ae94657a0021e7dc36": {
+			"id": "5fe778ae94657a0021e7dc36",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
